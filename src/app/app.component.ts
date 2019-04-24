84be192df8d00c903bdf8a87dcc17ea91ac15c8f
Eric Jones <seeriuss@gmail.com>
2019-04-24 13:48:29 -0600

    Build point.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, OnInit, AfterViewInit, ChangeDetectorRef } from "@angular/core";
+import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular";
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+
 
 @Component({
     moduleId: module.id,
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements AfterViewInit, OnInit {
+
+    constructor(private _changeDetectionRef: ChangeDetectorRef) {
+    }
+
+    @ViewChild(RadSideDrawerComponent) public drawerComponent: RadSideDrawerComponent;
+    private drawer: RadSideDrawer;
+
+    ngAfterViewInit() {
+    }
+
+    ngOnInit() {
+    }
+
+
+    set mainContentText(value: string) {
+    }
+
+    public openDrawer() {
+    }
+
+    public onCloseDrawerTap() {
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -10,7 +11,8 @@ import { AppComponent } from "./app.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptUISideDrawerModule
     ],
     declarations: [
         AppComponent
